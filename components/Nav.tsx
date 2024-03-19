468447ce15b5cd86f657717b35c4ecf4309c95f1
eliotourvieille <e.tourvieille@gmail.com>
2024-03-19 11:16:35 +0100

    Init de shadcn + install lucide react

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Nav() {
     return (
-        <nav>
-            Navigation
+        <nav className="flex-between w-full mb-16 p-9">
+            <Link href="/" className="flex gap-2 flex-center">
+                <Image src="/images/logo.png" alt="Logo Carnaval de Monthey" width={50} height={50} className="object-contain"/>
+                <p className="logo_text">Accueil</p>
+            </Link>
+
+            <Link href="/" className="flex gap-2 flex-center">
+                <p className="logo_text">Infos</p>
+            </Link>
         </nav>
     )
 }
